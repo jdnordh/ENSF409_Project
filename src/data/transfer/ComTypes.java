f2322b91d9d7258ec6c56b16f62e81a9990bd657
PCJORDAN\Jordan <jdnordh@gmail.com>
2017-04-04 21:16:53 -0600

    Small change

--- src/data/transfer/ComTypes.java
+++ src/data/transfer/ComTypes.java
@@ -23,6 +23,7 @@ public interface ComTypes {
 	public static final int FLIGHT_DELETE_CONFIRM = 15;
 	public static final int TICKET_DELETE_CONFIRM = 16;
 	public static final int FLIGHT_ADD_CONFIRM = 17;
+	
 	public static final int BAD_REQUEST = 50;
 	public static final int FAILED_GENERAL = 51;
 	public static final int FAILED_LOGIN = 52;
