d99e78b5658588f6d06661a7eb6d3622006ab335
DESKTOP-75J9T4O\jerem <jeremy.phillips@ucalgary.ca>
2017-04-04 19:52:34 -0600

    All gui work done for the most part

--- src/client/ClientGui.java
+++ src/client/ClientGui.java
@@ -1,75 +1,274 @@
 package client;
 
-import java.awt.GridBagConstraints;
-import java.awt.GridBagLayout;
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import javax.swing.*;
 
 public class ClientGui extends JFrame{
-	/** Panels */
-	private JPanel left;
-	private JPanel right;
-	private JPanel display;
+
+	private static final long serialVersionUID = 1L;
+
+	/** Frames */
+	static ClientGui g;
+	private JFrame loginWindow;
+	private JFrame newUserWindow;
+	private JOptionPane dialogFrame;
+
+	/** Combo box */
+	protected JComboBox<String> comboBoxDay;
+	protected char comboBoxSelectionDay;
+	protected JComboBox<String> comboBoxMonth;
+	protected char comboBoxSelectionMonth;
+	protected JComboBox<String> comboBoxYear;
+	protected char comboBoxSelectionYear;
 	
 	/** Buttons */
-	private JButton bookSeat;
-	private JButton cancelSeat;
-	private JButton search;
-	private JButton browse;
-	private JButton myTickets;
-	
-	/** editable text fields */
-	private JTextField searchText;
-	
-	/** uneditable text fields */
-	private JTextField messages;
-	private JTextField departsFrom;
-	private JTextField destination;
-	private JTextField date;
-	private JTextField duration;
-	private JTextField row;
-	private JTextField isle;
-	private JTextField price;
-	
-	
-	
-	public ClientGui(String user){
-		super(user);
-		this.makeLeftSide();
-		this.makeRightSide();
-		this.setVisible(true);
-	}
-	
-	private void makeLeftSide(){
-		left = new JPanel(new GridBagLayout());
-		ClientListener event = new ClientListener();
-		JPanel top = new JPanel();
-		
-		GridBagConstraints c = new GridBagConstraints();
-		c.gridx = 0;
-		c.gridy = 0;
-		
-	}
-	
-	private void makeRightSide(){
-		right = new JPanel();
-		ClientListener event = new ClientListener();
-		
-	}
-	
-	private class ClientListener implements ActionListener{
+	private JButton chooseLogin = new JButton("Login with Username");
+	private JButton newUser = new JButton("Create new login");
+	private JButton login;
+	private JButton cancelLogin;
+	private JButton createAccount;
+	private JButton cancelCreation;
 
-		@Override
-		public void actionPerformed(ActionEvent arg0) {
-			// TODO Auto-generated method stub
-			
+	/** editable text fields*/
+	private JTextField username;
+	private JTextField newUserFN;
+	private JTextField newUserLN;
+	private JTextField newUserUN;
+	
+	/**password fields*/
+	private JPasswordField password;
+	private JTextField newUserPassword;
+	private JPasswordField confirmUserPassword;
+
+	public AdminGui head;
+
+	
+	public ClientGui(){
+		this.setTitle("Setup");
+		this.setLayout(new GridLayout(2, 1));
+		this.setBounds(325, 225, 300, 115);
+		
+		this.add(createUpperPanel());
+		this.add(createCenterPanel());
+		
+		this.setVisible(true);
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
+	private JPanel createUpperPanel() {
+		JPanel upperPanel = new JPanel();
+		chooseLogin.addActionListener(new ClientListener());
+		upperPanel.add(chooseLogin);
+		return upperPanel;
+	}
+	
+	private JPanel createCenterPanel() {
+		JPanel centerPanel = new JPanel();
+		newUser.addActionListener(new ClientListener());
+		centerPanel.add(newUser);
+		return centerPanel;
+	}
+	
+	private void loginWindow()
+	{
+		loginWindow = new JFrame();
+		loginWindow.setLayout(new GridLayout(3, 1));
+		loginWindow.setTitle("Login");
+		loginWindow.setBounds(325, 225, 500, 150);
+		
+		//north
+		JPanel upperPanel = new JPanel();
+		JLabel label1 = new JLabel("Username:");
+		username = new JTextField(30);
+		upperPanel.add(label1);
+		upperPanel.add(username);
+		loginWindow.add(upperPanel);
+		
+		//center
+		JPanel centerPanel = new JPanel();
+		JLabel label2 = new JLabel("Password:");
+		password = new JPasswordField(30);
+		centerPanel.add(label2);
+		centerPanel.add(password);
+		loginWindow.add(centerPanel);
+		
+		//south
+		JPanel lowerPanel = new JPanel();
+		login = new JButton("Login");
+		login.addActionListener(new ClientListener());
+		lowerPanel.add(login);
+		cancelLogin = new JButton("Cancel");
+		cancelLogin.addActionListener(new ClientListener());
+		lowerPanel.add(cancelLogin);
+		loginWindow.add(lowerPanel);
+
+		loginWindow.setVisible(true);
+	}
+
+	private void newUserWindow() {
+		newUserWindow = new JFrame();
+		newUserWindow.setLayout(new GridLayout(7, 1));
+		newUserWindow.setTitle("New User");
+		newUserWindow.setBounds(325, 225, 450, 400);
+		newUserWindow.setVisible(true);
+		
+		//row one
+		JPanel rOne = new JPanel();
+		newUserFN = new JTextField(30);
+		rOne.add(new JLabel("First Name:"));
+		rOne.add(newUserFN);
+		//one.add(Box.createRigidArea(new Dimension(450 ,10)));
+		newUserWindow.add(rOne);
+
+		//row two
+		JPanel rTwo = new JPanel();
+		newUserLN = new JTextField(30);
+		rTwo.add(new JLabel("Last Name:"));
+		rTwo.add(newUserLN);
+		//rTwo.add(Box.createRigidArea(new Dimension(450 ,10)));
+		newUserWindow.add(rTwo);
+
+		//row three
+		JPanel rThree = new JPanel();
+		newUserUN = new JTextField(30);
+		rThree.add(new JLabel("Username:"));
+		rThree.add(newUserUN);
+		//rThree.add(Box.createRigidArea(new Dimension(450 ,10)));
+		newUserWindow.add(rThree);
+
+		//row four
+		JPanel rFour = new JPanel();
+		newUserPassword = new JPasswordField(30);
+		rFour.add(new JLabel("Password:"));
+		rFour.add(newUserPassword);
+		rFour.add(Box.createRigidArea(new Dimension(450 ,10)));
+		newUserWindow.add(rFour);
+		
+		//row five
+		JPanel rFive = new JPanel();
+		confirmUserPassword = new JPasswordField(25);
+		rFive.add(new JLabel("Confirm Password:"));
+		rFive.add(confirmUserPassword);
+		rFive.add(Box.createRigidArea(new Dimension(450 ,10)));
+		newUserWindow.add(rFive);
+		
+		//row six
+		JPanel rSix = new JPanel();
+		rSix.add(new JLabel("Date of Birth:"));
+		rSix.add(Box.createRigidArea(new Dimension(10 ,10)));
+		rSix.add(new JLabel("Day:"));
+		String [] temp1 = new String[31];
+		for(int i = 0, j = 1; i < temp1.length; i++, j++)
+			temp1[i] = Integer.toString(j);
+		comboBoxDay = new JComboBox<String>(temp1);
+		comboBoxDay.setEditable(false);
+		//comboBox.addActionListener(listen);
+		rSix.add(comboBoxDay);
+		rSix.add(Box.createRigidArea(new Dimension(10 ,10)));
+
+		rSix.add(new JLabel("Month:"));
+		String [] temp2 = new String[12];
+		for(int i = 0, j = 1; i < temp2.length; i++, j++)
+			temp2[i] = Integer.toString(j);
+		comboBoxMonth = new JComboBox<String>(temp2);
+		comboBoxMonth.setEditable(false);
+		//comboBox.addActionListener(listen);
+		rSix.add(comboBoxMonth);
+		rSix.add(Box.createRigidArea(new Dimension(10 ,10)));
+
+		rSix.add(new JLabel("Year:"));			
+		String [] temp3 = new String[83];
+		for(int i = 0, j = 1935; i < temp3.length; i++, j++)
+			temp3[i] = Integer.toString(j);
+		comboBoxYear = new JComboBox<String>(temp3);
+		comboBoxYear.setEditable(false);
+		//comboBox.addActionListener(listen);
+		rSix.add(comboBoxYear);
+		rSix.add(Box.createRigidArea(new Dimension(10 ,10)));
+		newUserWindow.add(rSix);
+
+		//row seven
+		JPanel rSeven = new JPanel();
+		createAccount = new JButton("Create Account");
+		createAccount.addActionListener(new ClientListener());
+		rSeven.add(createAccount);
+		cancelCreation = new JButton("Cancel");
+		cancelCreation.addActionListener(new ClientListener());
+		rSeven.add(cancelCreation);
+		newUserWindow.add(rSeven);
+
 		}
 		
+	private class ClientListener implements ActionListener
+	{
+
+		public void actionPerformed(ActionEvent e) {
+			if (e.getSource() == chooseLogin) {
+				g.dispose();
+				loginWindow();
+			}
+			
+			else if (e.getSource() == newUser) {
+				g.dispose();
+				newUserWindow();
+			}
+			
+			else if(e.getSource() == login) {
+				
+				
+				if(username.getText().equals("Admin1") || username.getText().equals("Admin2"))
+				{
+					if(password.getText().equals("pass"))
+					{
+						loginWindow.dispose();
+						new AdminGui(null);
+					}
+					else
+					{
+						JOptionPane.showMessageDialog(dialogFrame, "Password Incorrect.");
+						password.setText("");
+					}
+				}
+				else
+					new CustomerGui();
+								
+			}
+			
+			else if(e.getSource() == cancelLogin) {
+				loginWindow.dispose();
+				g = new ClientGui();
+			}
+			
+			else if(e.getSource() == createAccount) {
+				if(newUserFN.getText().equals("") || newUserLN.getText().equals("") ||
+						newUserUN.getText().equals("") || newUserUN.getText().equals("") || 
+						newUserPassword.getText().equals("") || confirmUserPassword.getText().equals("") )
+				{
+					JOptionPane.showMessageDialog(dialogFrame, "One or More of the Required Fields are Blank.");
+
+				}
+				//create login
+				newUserWindow.dispose();
+				g = new ClientGui();
+			}
+			
+			else if(e.getSource() == cancelCreation) {
+				newUserWindow.dispose();
+				g = new ClientGui();
+			}
+			
+		}
+
+		
 	}
 	
-	public static void main(){
-		ClientGui g = new ClientGui("john68");
+	public static void main(String[] args){
+		g = new ClientGui();
 	}
 }
---
+++ src/client/CustomerGui.java
@@ -0,0 +1,274 @@
+package client;
+
+import java.awt.Color;
+import java.awt.ComponentOrientation;
+import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.GridLayout;
+
+import javax.swing.*;
+
+
+
+
+public class CustomerGui extends JFrame {
+	
+	private CustomerGui cust;
+
+	/** Combo box */
+	protected JComboBox<String> comboBoxSearch;
+	protected char comboBoxSelectionDay1;
+	
+	/** editable text fields*/
+	private JTextField searchFlights;
+	private JTextField flightNumField;
+	private JTextField departureField;
+	private JTextField destField;
+	private JTextField depDate;
+	private JTextField depTime;
+	private JTextField durationField;
+	private JTextField totSeatsField;
+	private JTextField remSeatsField;
+	private JTextField priceField;
+
+	
+	/**buttons */
+	private JButton searchButton;
+	private JButton clearButton;
+	private JButton myBookings;
+	private JButton bookFlight;
+	private JButton clearFlight;
+
+	/** This is the list of clients */
+	protected JList<Client> display;
+	private DefaultListModel<Client> list;
+	
+	protected JTextArea textArea;
+	
+	public CustomerGui() {
+		this.setTitle("Welcome Customer");
+		this.setBounds(325, 225, 725, 600);
+		this.setLayout(new GridLayout(2, 2));
+		
+		this.add(topLeftPanel());
+		this.add(topRightPanel());
+		this.add(botLeftPanel());
+		this.add(botRightPanel());
+		
+		this.setVisible(true);
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
+	private JPanel topLeftPanel() {
+		JPanel panel = new JPanel();
+		panel.setLayout(new GridLayout(4, 1));
+		
+		//row one
+		JPanel rOne = new JPanel();
+		myBookings = new JButton("My Bookings");
+		rOne.add(myBookings);		
+		panel.add(rOne);
+		
+		//row two
+		JPanel rTwo = new JPanel();
+		rTwo.add(new JLabel("Search Flights by:"));
+		rTwo.add(Box.createRigidArea(new Dimension(10, 10)));
+		String [] temp1 = {"Date", "Source", "Destination"};
+		comboBoxSearch = new JComboBox<String>(temp1);
+		comboBoxSearch.setEditable(false);
+		//comboBox.addActionListener(listen);
+		rTwo.add(comboBoxSearch);
+		panel.add(rTwo);
+		
+		//row two
+		JPanel rThree = new JPanel();
+		rThree.add(new JLabel("Search:"));
+		rThree.add(Box.createRigidArea(new Dimension(10, 10)));
+		searchFlights = new JTextField(20);
+		rThree.add(searchFlights);
+		panel.add(rThree);
+		
+		//row three
+		JPanel rFour = new JPanel();
+		rFour.add(Box.createRigidArea(new Dimension(10, 10)));
+		searchButton = new JButton("Search");
+		rFour.add(searchButton);
+		rFour.add(Box.createRigidArea(new Dimension(10, 10)));
+		clearButton = new JButton("Clear");
+		rFour.add(clearButton);
+		panel.add(rFour);
+
+		panel.setBorder(BorderFactory.createLineBorder(Color.black));
+		return panel;
+	}
+
+	
+
+	private JPanel topRightPanel() {
+		JPanel panel = new JPanel();
+		panel.setLayout(new GridLayout(5, 1));
+		
+		//row one
+		JPanel rOne = new JPanel();
+		rOne.add(new JLabel("Flight Number:"));
+		rOne.add(Box.createRigidArea(new Dimension(5, 10)));
+		flightNumField = new JTextField(20);
+		flightNumField.setEditable(false);
+		flightNumField.setBackground(Color.white);
+		rOne.add(flightNumField);
+		rOne.add(Box.createRigidArea(new Dimension(10, 10)));
+		panel.add(rOne);
+		
+		//row two
+		JPanel rTwo = new JPanel();
+		rTwo.add(new JLabel("Departs From:"));
+		rTwo.add(Box.createRigidArea(new Dimension(5, 10)));
+		departureField = new JTextField(20);
+		departureField.setEditable(false);
+		departureField.setBackground(Color.white);
+		rTwo.add(departureField);
+		rTwo.add(Box.createRigidArea(new Dimension(10, 10)));
+		panel.add(rTwo);
+		
+		//row three
+		JPanel rThree = new JPanel();
+		rThree.add(new JLabel("Destination:"));
+		rThree.add(Box.createRigidArea(new Dimension(5, 10)));
+		destField = new JTextField(20);
+		destField.setEditable(false);
+		destField.setBackground(Color.white);
+		rThree.add(destField);
+		rThree.add(Box.createRigidArea(new Dimension(10, 10)));
+		panel.add(rThree);
+		
+		//row four
+		JPanel rFour = new JPanel();
+		rFour.add(new JLabel("Departure Date:"));
+		rFour.add(Box.createRigidArea(new Dimension(5, 10)));
+		depDate = new JTextField(20);
+		depDate.setEditable(false);
+		depDate.setBackground(Color.white);
+		rFour.add(depDate);
+		rFour.add(Box.createRigidArea(new Dimension(10, 10)));
+		panel.add(rFour);
+		
+		//row five
+		JPanel rFive = new JPanel();
+		rFive.add(new JLabel("Departure Time:"));
+		rFive.add(Box.createRigidArea(new Dimension(5, 10)));
+		depTime = new JTextField(20);
+		depTime.setEditable(false);		
+		depTime.setBackground(Color.white);
+		rFive.add(depTime);
+		rFive.add(Box.createRigidArea(new Dimension(10, 10)));
+		panel.add(rFive);
+		
+		return panel;
+	}
+
+
+	private JPanel botRightPanel() {
+		JPanel panel = new JPanel();
+		panel.setLayout(new GridLayout(5, 1));
+		
+		//row one
+		JPanel rOne = new JPanel();
+		rOne.add(new JLabel("Flight Duration:"));
+		rOne.add(Box.createRigidArea(new Dimension(5, 10)));
+		durationField = new JTextField(20);
+		durationField.setEditable(false);
+		durationField.setBackground(Color.white);
+		rOne.add(durationField);
+		rOne.add(Box.createRigidArea(new Dimension(10, 10)));
+		panel.add(rOne);
+		
+		//row two
+		JPanel rTwo = new JPanel();
+		rTwo.add(new JLabel("Total Seats:"));
+		rTwo.add(Box.createRigidArea(new Dimension(5, 10)));
+		totSeatsField = new JTextField(20);
+		totSeatsField.setEditable(false);
+		totSeatsField.setBackground(Color.white);
+		rTwo.add(totSeatsField);
+		rTwo.add(Box.createRigidArea(new Dimension(10, 10)));
+		panel.add(rTwo);
+		
+		//row three
+		JPanel rThree = new JPanel();
+		rThree.add(new JLabel("Remaining Seats:"));
+		rThree.add(Box.createRigidArea(new Dimension(5, 10)));
+		remSeatsField = new JTextField(20);
+		remSeatsField.setEditable(false);
+		remSeatsField.setBackground(Color.white);
+		rThree.add(remSeatsField);
+		rThree.add(Box.createRigidArea(new Dimension(10, 10)));
+		panel.add(rThree);
+
+		//row four
+		JPanel rFour = new JPanel();
+		rFour.add(new JLabel("Price:"));
+		rFour.add(Box.createRigidArea(new Dimension(5, 10)));
+		priceField = new JTextField(20);
+		priceField.setEditable(false);
+		priceField.setBackground(Color.white);
+		rFour.add(priceField);
+		rFour.add(Box.createRigidArea(new Dimension(10, 10)));
+		panel.add(rFour);
+		
+		//row five
+		JPanel rFive = new JPanel();
+		bookFlight = new JButton("Book");
+		rFive.add(bookFlight);
+		rFive.add(Box.createRigidArea(new Dimension(10, 10)));
+		clearFlight = new JButton("Clear");
+		rFive.add(clearFlight);
+		panel.add(rFive);
+
+		
+		return panel;
+	}
+
+
+	private JPanel botLeftPanel() {
+		JPanel panel = new JPanel();
+		
+		list = new DefaultListModel<Client>();
+		display = new JList<Client>(list);
+		display.setFont(new Font("Courier New", Font.BOLD, 12));
+		//display.addListSelectionListener(new ListAction());
+		display.setSelectionMode(ListSelectionModel.SINGLE_INTERVAL_SELECTION);
+		display.setLayoutOrientation(JList.HORIZONTAL_WRAP);
+		display.setVisibleRowCount(-1);
+		JScrollPane textAreaScrollPane = new JScrollPane();
+		textArea = new JTextArea();
+		textAreaScrollPane.setComponentOrientation(ComponentOrientation.RIGHT_TO_LEFT);
+		textArea.setFont(new Font("Courier New", Font.BOLD, 12));
+		textArea.setEditable(false);
+		textAreaScrollPane = new JScrollPane(display);
+		textAreaScrollPane.setPreferredSize(new Dimension(325, 260));
+		panel.add(textAreaScrollPane);
+
+		panel.setBorder(BorderFactory.createLineBorder(Color.black));
+		return panel;
+	}
+
+
+	/*private class ListAction implements ListSelectionListener {
+
+		@SuppressWarnings("unchecked")
+		@Override
+		public void valueChanged(ListSelectionEvent e) {
+			
+			//client = ((JList<Client>)e.getSource()).getSelectedValue();
+			//client.displayInfo(clientIDIn, firstNameIn, lastNameIn, addressIn, postalIn, phoneNumIn, comboBox);
+			//dThread.setClient(client);
+		}
+
+		
+	}*/
+
+	public static void main(String[] args) {
+		//CustomerGui g = new CustomerGui();
+	}
+
+}
